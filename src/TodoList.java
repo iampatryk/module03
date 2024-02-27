fc0d25f7ac02e2421db13803aa2bac1e10ffcf11
iampatryk <mpatrykmiller@gmail.com>
2024-02-27 21:38:16 +0100

    prep task TodoList

---
+++ src/TodoList.java
@@ -0,0 +1,25 @@
+public class TodoList {
+    public void add(String task) {
+    //Dodaj zadanie do listy
+
+    }
+
+    public void print() {
+    //Wydrukuj zadania z listy wraz z ich numerem na liście
+
+    }
+
+    public void remove(int number) {
+    //Usuń zadanie z podanego numeru
+
+    }
+}
+
+
+//TodoList: Stwórz klasę TodoList. Powinna zawierać konstruktor bez parametrów,
+// który inicjalizuje wewnętrzną tablicę do przechowywania rzeczy do zrobienia i
+// następujące metody:
+//
+//public void add(String task) - Dodaj zadanie do listy;
+//public void print() - Wydrukuj zadania z listy wraz z ich numerem na liście;
+//public void remove(int number) - Usuń zadanie z podanego numeru.
