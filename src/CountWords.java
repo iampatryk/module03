e8d4d26ff9a90e00137be6553263abc532a359dc
iampatryk <mpatrykmiller@gmail.com>
2024-03-01 21:45:48 +0100

    CountWords . update

--- src/CountWords.java
+++ src/CountWords.java
@@ -1,12 +1,39 @@
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//public class CountWords {
-//        public HashMap<String, Integer> countWords(String text) {
-//
-//
-//        }
-//}
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountWords {
+
+    public void wordCounter() {
+
+        String text = "Ogólnie tekst znana teza głosi, iż tekst użytkownika może rozpraszać zrozumiała zawartość strony, kiedy ten chce zobaczyć sam jej wygląd. Jedną z mocnych stron używania Lorem Ipsum jest to, że ma wiele różnych „kombinacji” zdań, słów i akapitów, w przeciwieństwie do zwykłego: „tekst, tekst, tekst”, sprawiającego, że wygląda to „zbyt czytelnie” po polsku. Wielu webmasterów i designerów używa Lorem Ipsum jako domyślnego modelu tekstu i wpisanie w internetowej wyszukiwarce ‘lorem ipsum’ spowoduje znalezienie bardzo wielu stron, które wciąż są w budowie. Wiele wersji tekstu ewoluowało i zmieniało się przez lata, czasem przez przypadek, czasem specjalnie (humorystyczne wstawki itd).";
+        String word = "tekst";
+        String[] separateText = text.split(word);
+        int numberWord = separateText.length - 1;
+        System.out.println("Word: " + word + " it's use " + numberWord + " times.");
+
+    }
+
+    public static HashMap<String, Integer> countWords(String text) {
+        // podzielenie tekstu
+        String[] words = text.split("\\s+");
+        //     // Inicjalizacja pustej mapy, gdzie kluczem jest słowo, a wartością ilość jego wystąpień.
+        HashMap<String, Integer> wordCountMap = new HashMap<>();
+
+        for (String word : words) {
+            // Metoda merge z HashMap pozwala na łączenie wartości dla danego klucza.
+            // Jeśli klucz (słowo) już istnieje w mapie, użyj funkcji Integer::sum, aby zwiększyć liczbę wystąpień o 1.
+            // Jeśli klucz nie istnieje, dodaj nowy klucz z wartością 1.
+            wordCountMap.merge(word, 1, Integer::sum);
+        }
+//        return wordCountMap;
+        return wordCountMap;
+    }
+}
+
+
+//Zatem wyrażenie text.split("\\s+") dzieli tekst na słowa, traktując dowolny biały znak jako separator
+// pomiędzy nimi. Na przykład, jeśli mamy tekst "Ala ma kota", to po użyciu split("\\s+") otrzymamy tablicę
+// zawierającą trzy elementy: ["Ala", "ma", "kota"].
 
 // Liczenie słów w tekście: Napisz metodę, która przyjmując String,
 // zwróci HashMapę, w której kluczem jest słowo, a wartością ilość
--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,51 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        Student s1 = new Student("Wood",23,199,"Alex");
-        Student s2 = new Student("Pink",34,160,"Jo");
-        Student s3 = new Student("Shmit",18,179,"Paul");
-        Student s4 = new Student("Scott",54,184,"Eva");
-        Student s5 = new Student("Amber",43,182,"Chris");
+//        Student s1 = new Student("Wood",23,199,"Alex");
+//        Student s2 = new Student("Pink",34,160,"Jo");
+//        Student s3 = new Student("Shmit",18,179,"Paul");
+//        Student s4 = new Student("Scott",54,184,"Eva");
+//        Student s5 = new Student("Amber",43,182,"Chris");
+//
+//        List<Student> studentList = new ArrayList<>();
+//
+//        studentList.add(s1);
+//        studentList.add(s2);
+//        studentList.add(s3);
+//        studentList.add(s4);
+//        studentList.add(s5);
+//
+//        System.out.println("Print List");
+//
+//        for(Student student : studentList) {
+//            System.out.println(student);
+//        }
+//
+//        System.out.println("After default sorting");
+//
+//        Collections.sort(studentList);
+//
+//        for(Student student : studentList) {
+//            System.out.println(student);
+//        }
+//
+//        System.out.println("After sorting by lastName");
+//
+//        StudentLastNameComparator comparator = new StudentLastNameComparator();
+//        Collections.sort(studentList, comparator);
+//
+//        for(Student student : studentList) {
+//            System.out.println(student);
+//        }
 
-        List<Student> studentList = new ArrayList<>();
 
-        studentList.add(s1);
-        studentList.add(s2);
-        studentList.add(s3);
-        studentList.add(s4);
-        studentList.add(s5);
 
-        System.out.println("Print List");
 
-        for(Student student : studentList) {
-            System.out.println(student);
-        }
-
-        System.out.println("After default sorting");
-
-        Collections.sort(studentList);
-
-        for(Student student : studentList) {
-            System.out.println(student);
-        }
-
-        System.out.println("After sorting by lastName");
-
-        StudentLastNameComparator comparator = new StudentLastNameComparator();
-        Collections.sort(studentList, comparator);
-
-        for(Student student : studentList) {
-            System.out.println(student);
-        }
 
 //        SummingTheArray mySumArr = new SummingTheArray();
 //        mySumArr.sumaTablicy();
@@ -55,5 +60,26 @@ public class Main {
 //        myTodoList.remove(3);
 //        myTodoList.print();
 
+
+
+//        CountWords myCunter = new CountWords();
+//        myCunter.wordCounter();
+
+
+
+        String text1 = "But all I must explain to you all how all this mistaken idea of denouncing pleasure and all praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness all. No one rejects, dislikes, or all avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because all it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it?";
+        String word = "all";
+
+        HashMap<String, Integer> map = CountWords.countWords(text1);
+
+        int numberWord = map.getOrDefault(word,0);
+        System.out.println("Word: " + word + " it's use " + numberWord + " times.");
+
+
+
     }
+
+//    private static HashMap<String, Integer> countWords(String tekst) {
+//        return null;
+//    }
 }
