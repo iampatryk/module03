24fc050b607a9ee0482b3577995b86929605990d
iampatryk <mpatrykmiller@gmail.com>
2024-03-14 21:39:23 +0100

    unit tests

--- src/Main.java
+++ src/Main.java
@@ -80,7 +80,7 @@ public class Main {
 
 
             String text = "But all I must all, all. explain to you all how all this mistaken idea of denouncing pleasure and all praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness all. No one rejects, dislikes, or all avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because all it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it?";
-            String searchWord = "all";
+//            String searchWord = "all";
 
             HashMap<String, Integer> wordCountMap = CountWords.countWords(text);
 
