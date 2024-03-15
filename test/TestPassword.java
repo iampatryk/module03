650ce216f187dbbdc3bb76b927a7ed8ca2ef8201
iampatryk <mpatrykmiller@gmail.com>
2024-03-15 20:20:51 +0100

    unit tests

---
+++ test/TestPassword.java
@@ -0,0 +1,13 @@
+public class TestPassword {
+}
+
+
+//Używając metodologii TDD napisz testy oraz klasę weryfikującą hasła,
+// która sprawdza, czy hasło ma długość co najmniej 12 znaków,
+// nie ma białych znaków, żadne znaki koło siebie się nie powtarzają,
+// zawiera co najmniej jeden numer oraz znak specjalny(!.@-;”(){}[]?):
+//public boolean hasGoodLength(String password);
+//public boolean hasNoWhiteSpaces(String password);
+//public boolean hasNoRepetitions(String password);
+//public boolean hasNumbers(String password);
+//public boolean hasSpecialCharacter(String password).
